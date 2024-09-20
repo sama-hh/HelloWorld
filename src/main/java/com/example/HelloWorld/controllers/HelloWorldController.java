5aa165b17e12bce864e5b130f808cf9dc55167b4
sama-hh <samahajimuradovah@gmail.com>
2024-09-20 10:53:07 +0200

    add path variable

--- src/main/java/com/example/HelloWorld/controllers/HelloWorldController.java
+++ src/main/java/com/example/HelloWorld/controllers/HelloWorldController.java
@@ -1,14 +1,15 @@
 package com.example.HelloWorld.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-    @GetMapping("/api/hello")
-    public String helloWorld() {
-        return "Hello World!";
+    @GetMapping("/api/hello/{name}")
+    public String helloWorld(@PathVariable String name) {
+        return "Hello " + name;
     }
 
 }
